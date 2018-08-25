b6461eda3f5fe6a8d9426f739067fc491029aee6
kakuei <kakuei.araral@gmail.com>
2018-08-26 00:21:46 +0800

    js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function init() {
+Tabletop.init( { key: ‘1sSqdGzobbySAUfJw9YKFS-PrfU8rz7a-XBSUgbTgl7U’,
+callback: showInfo,
+simpleSheet: true } )
+}
+function showInfo(data, tabletop) {
+console.log(data);
+for (var i = 0; i < data.length; i++) {
+$(‘.post’).append(
+‘<div class=”text”>’ +
+‘<h1>’ + data[i].title + ‘</h1>’+
+data[i].body +
+‘</div> ‘ + ‘<div class=”meta-info”>’ + 
+‘<div class=”date”>’ + data[i].date + ‘</div>’ +
+‘</div>’ +
+‘</div>’);
+}
+}
+window.addEventListener(‘DOMContentLoaded’, init)
